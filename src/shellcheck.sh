44ddbd4565052b8c7dd72bf31033335b1f0f8c90
opsa <>
2021-06-19 06:49:09 +0000

    ci: added output for shellcheck
    FossilOrigin-Name: 8e08f2368ac331e4edc77e53e6e3df808e4047b2eb32748652256a7454fa8e89

--- src/shellcheck.sh
+++ src/shellcheck.sh
@@ -17,10 +17,12 @@ done < <(
 # shell_check.sh \
 # dockerfilecheck.sh \
 
+echo "Shellcheck main files"
 LC_ALL=C.UTF-8 shellcheck "${targets[@]}"
 
 # VERSION were exclude from main check (above)
 # exclude warning https://github.com/koalaman/shellcheck/wiki/SC2034
+echo "Shellcheck VERSION"
 LC_ALL=C.UTF-8 shellcheck --shell=sh --exclude=SC2034 VERSION
 
 exit $?
