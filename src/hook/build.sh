e521de2b038b01fa5a068b8c4a913796c09edd57
oupfiz5 <>
2022-08-30 20:18:19 +0000

    used buildx for build docker image
    FossilOrigin-Name: f852bb724ad0a4cdddb52a3d3c0a9910df6f6f6a7e5714a42916e6a41ecb930f

--- src/hook/build.sh
+++ src/hook/build.sh
@@ -5,7 +5,7 @@ set -a; source ../VERSIONS ; set +a;
 IMAGE="${IMAGE:-${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}}"
 DOCKERFILE="${DOCKERFILE:-Dockerfile}"
 
-docker build \
+docker buildx build \
        --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
        --build-arg S6_OVERLAY_VERSION="${S6_OVERLAY_VERSION}" \
        --build-arg UBUNTU_VERSION="${UBUNTU_VERSION}" \
